71190d01a71f9b8fc768d158d9d0a97d00f0b6ca
Kabir <kabirbaidhya@gmail.com>
2016-02-21 21:50:31 +0545

    Setup eslint for ES6

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,21 @@
 var gulp = require('gulp');
 var babelify = require('babelify');
 var uglify = require('gulp-uglify');
+var eslint = require('gulp-eslint');
 var rename = require('gulp-rename');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
-gulp.task('compile', function() {
+// Lint using eslint
+gulp.task('lint', function() {
+    return gulp.src(['**/*.js','!node_modules/**', '!dist/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+// Compile ES6
+gulp.task('compile', ['lint'], function() {
     return browserify({ entries: './src/H.js', debug: true })
         .transform(babelify)
         .bundle()
@@ -14,14 +24,15 @@ gulp.task('compile', function() {
         .pipe(gulp.dest('dist'));
 });
 
+// Uglify
 gulp.task('uglify', ['compile'], function() {
-    gulp.src('dist/h.js')
+    return gulp.src('dist/h.js')
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['compile', 'uglify']);
+gulp.task('default', ['lint', 'compile', 'uglify']);
 
 gulp.task('watch', function() {
     gulp.watch('./src/**/*.js', ['default']);
