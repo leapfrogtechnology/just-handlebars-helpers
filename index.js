affc2168d4ecf9b04d4e0106b4cb7c21ac17f439
Saugat Acharya <mesaugat@gmail.com>
2016-02-28 09:21:19 +0545

    Update comment

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-// Note: the ES6 export default would export that H class in 'default' key
-// so we have to use that
+// Note: ES6 export default would export the H class in 'default' key so we have to use that
 module.exports = require('./lib/H.js').default;
