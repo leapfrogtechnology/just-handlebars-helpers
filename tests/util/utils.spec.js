cb4d6c8fa5a799e82cfaf5bb7637d1fbf72b6714
Kabir <kabirbaidhya@gmail.com>
2016-03-16 22:38:06 +0545

    Beautify util specs

--- tests/util/utils.spec.js
+++ tests/util/utils.spec.js
@@ -1,53 +1,59 @@
 import * as utils from '../../src/util/utils.js';
 
 describe('utils', () => {
+    describe('isFunction', () => {
+        it('should return true if param is a function', () => {
+            expect(utils.isFunction(parseInt)).toEqual(true);
+        });
 
-    /* isFunction */
-    it('isFunction should return true if param is a function', () => {
-        expect(utils.isFunction(parseInt)).toEqual(true);
+        it('should return false if param is not a function', () => {
+            expect(utils.isFunction(window)).toEqual(false);
+        });
     });
 
-    it('isFunction should return false if param is not a function', () => {
-        expect(utils.isFunction(window)).toEqual(false);
+    describe('isUndefined', () => {
+        it('should return true if param is undefined', () => {
+            expect(utils.isUndefined(undefined)).toEqual(true);
+        });
+
+        it('should return false if param is not undefined', () => {
+            expect(utils.isUndefined('notUndefined')).toEqual(false);
+        });
     });
 
-    /* isUndefined */
-    it('isUndefined should return true if param is undefined', () => {
-        expect(utils.isUndefined(undefined)).toEqual(true);
+    describe('isDefined', () => {
+        it('should return true if param is not undefined', () => {
+            expect(utils.isDefined('notUndefined')).toEqual(true);
+        });
+
+        it('should return false if param is defined', () => {
+            expect(utils.isDefined(undefined)).toEqual(false);
+        });
     });
 
-    it('isUndefined should return false if param is not undefined', () => {
-        expect(utils.isUndefined('notUndefined')).toEqual(false);
+    describe('isObject', () => {
+        it('should return true if param is an object', () => {
+            expect(utils.isObject(window)).toEqual(true);
+        });
+
+        it('should return false if param is not an object', () => {
+            expect(utils.isObject(parseInt)).toEqual(false);
+        });
     });
 
-    /* isDefined */
-    it('isDefined should return true if param is not undefined', () => {
-        expect(utils.isDefined('notUndefined')).toEqual(true);
-    });
+    describe('isArray', () => {
+        it('should return true if param passed is an array', () => {
+            expect(utils.isArray([5, 6, 7])).toEqual(true);
+        });
 
-    it('isDefined should return false if param is defined', () => {
-        expect(utils.isDefined(undefined)).toEqual(false);
-    });
+        it('should return false if param is a normal object', () => {
+            expect(utils.isArray({
+                foo: 'bar'
+            })).toEqual(false);
+        });
 
-    /* isObject */
-    it('isObject should return true if param is an object', () => {
-        expect(utils.isObject(window)).toEqual(true);
-    });
-
-    it('isObject should return false if param is not an object', () => {
-        expect(utils.isObject(parseInt)).toEqual(false);
-    });
-
-    /* isArray */
-    it('isArray should return true if param passed is an array', () => {
-        expect(utils.isArray([5, 6, 7])).toEqual(true);
-    });
-
-    it('isArray should return false if param is a normal object', () => {
-        expect(utils.isArray({foo:'bar'})).toEqual(false);
-    });
-
-    it('isArray should return false if param is a scalar value', () => {
-        expect(utils.isArray(567)).toEqual(false);
+        it('should return false if param is a scalar value', () => {
+            expect(utils.isArray(567)).toEqual(false);
+        });
     });
 });
