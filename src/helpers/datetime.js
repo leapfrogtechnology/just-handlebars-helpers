029336e9764a90afe969e1994339811be5c960d2
rhanton <rick.hanton@kubra.com>
2020-07-17 12:24:05 -0700

    Adding locale based date formatting helper.

--- src/helpers/datetime.js
+++ src/helpers/datetime.js
@@ -21,3 +21,30 @@ export function formatDate(formatString, date) {
 
   return moment(date || new Date()).format(formatString);
 }
+
+/**
+ * A formatLocaleDate helper to format date with locale using moment js.
+ *
+ * @example
+ *      {{formatDate 'MM/DD/YYYY' 'es' date}}
+ *
+ * @param {string} formatString Based on moment.js.
+ * @param {string} localeString Language or language-country locale string available in https://github.com/moment/moment/tree/develop/locale .
+ * @param {Date} date
+ * @returns {string}
+ */
+export function formatLocaleDate(formatString, localeString, date) {
+  let moment = global.moment;
+
+  if (!moment) {
+    moment = require('moment');
+  }
+
+  formatString = isString(formatString) ? formatString : '';
+
+  if (isString(localeString)) {
+    moment.locale(localeString);
+  }
+
+  return moment(date || new Date()).format(formatString);
+}
--- tests/helpers/datetime.spec.js
+++ tests/helpers/datetime.spec.js
@@ -61,4 +61,67 @@ describe('datetime', () => {
     });
   });
 
+  describe('formatLocaleDate', () => {
+    it('should return locale-formatted day of week (dddd) even if date is not provided', () => {
+      const expectedDayOfWeek = datetime.formatLocaleDate('dddd', 'en', new Date());
+
+      expect(datetime.formatLocaleDate('dddd')).toEqual(expectedDayOfWeek);
+    });
+
+    it('should return locale-formatted day of week (dddd) in different lang even if date is not provided', () => {
+      const expectedDayOfWeek = datetime.formatLocaleDate('dddd', 'ku', new Date()); // ku = kurdish
+
+      expect(datetime.formatLocaleDate('dddd', 'ku')).toEqual(expectedDayOfWeek);
+    });
+
+    it('should return locale-formatted date (dddd) even if locale=null is provided', () => {
+      const expectedDayOfWeek = datetime.formatLocaleDate('dddd', 'en', new Date());
+
+      expect(datetime.formatLocaleDate('dddd', null)).toEqual(expectedDayOfWeek);
+    });
+
+
+    it('should rreturn locale-formatted date (dddd) even if date=null and locale=null is provided', () => {
+      const expectedDayOfWeek = datetime.formatLocaleDate('dddd', 'en', new Date());
+
+      expect(datetime.formatLocaleDate('dddd', null, null)).toEqual(expectedDayOfWeek);
+    });
+
+    it('should return locale-formatted date (dddd) even if locale=undefined is provided', () => {
+      const expectedDayOfWeek = datetime.formatLocaleDate('dddd', 'en', new Date());
+
+      expect(datetime.formatLocaleDate('dddd', undefined)).toEqual(expectedDayOfWeek);
+    });
+
+
+    it('should rreturn locale-formatted date (dddd) even if date=undefined and locale=undefined is provided', () => {
+      const expectedDayOfWeek = datetime.formatLocaleDate('dddd', 'en', new Date());
+
+      expect(datetime.formatLocaleDate('dddd', undefined, undefined)).toEqual(expectedDayOfWeek);
+    });
+
+    it('should return use the current date if no date is provided', () => {
+      const expected = datetime.formatLocaleDate('MMMM Do, YYYY', 'es', new Date());
+
+      expect(datetime.formatLocaleDate('MMMM Do, YYYY', 'es')).toEqual(expected);
+    });
+
+    it('should return german locale-formatted date if date is provided and formatting requests german', () => {
+      const actual = '01/02/2016';
+      const expected = 'Samstag, 2. Januar 2016 00:00';
+
+      expect(datetime.formatLocaleDate('LLLL', 'de', new Date(actual))).toEqual(expected);
+    });
+
+    it('should still work and return the full timestamp if no parameters are provided', () => {
+      expect(datetime.formatLocaleDate()).toMatch(/^\d{4}-\d{2}-\d{2}.+$/);
+    });
+
+    it('helper after compilation should return the current Date if no date is provided', () => {
+      const expected = datetime.formatLocaleDate('YYYY-MM-DD', new Date());
+
+      expect(datetime.formatLocaleDate('YYYY-MM-DD')).toEqual(expected);
+    });
+  });
+
 });
